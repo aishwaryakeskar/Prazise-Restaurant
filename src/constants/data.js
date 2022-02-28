2ef635681b391d8005515b030cd7b8990b701342
Aishwarya Keskar <aishwaryakeskar98@gmail.com>
2022-02-28 21:34:15 +0530

    Add Menu

---
+++ src/constants/data.js
@@ -0,0 +1,82 @@
+import images from './images';
+
+const appetizers = [
+  {
+    title: 'Clams Kimsino',
+    price: '$26',
+    tags: 'Bacon-Kimchi Butter, Breadcrumbs',
+  },
+  {
+    title: 'Seared Foie Gras',
+    price: '$29',
+    tags: 'Apples, Caramel Cider Gastrique, Rossa Onion, Seeded Rye Sourdough',
+  },
+  {
+    title: 'Oyster Rockefeller',
+    price: '$28',
+    tags: 'Greens, Shallot Butter, Absinthe',
+  },
+  {
+    title: 'Golden Beet Salad',
+    price: '$19',
+    tags: 'Pistachios, Sesame, Yogurt',
+  },
+  {
+    title: 'Cured Salmon',
+    price: '$26',
+    tags: 'Gribiche, Salmon Roe, Pumpernickel, Caper Berries',
+  },
+];
+
+const mains = [
+  {
+    title: 'Tortiglioni',
+    price: '$32',
+    tags: 'Butternut Squash, Mushroom, Parmesan',
+  },
+  {
+    title: "Mushroom Pot Pie",
+    price: '$32',
+    tags: 'Cannellini Beans, Kale, Thyme',
+  },
+  {
+    title: 'Grilled Black Bass',
+    price: '$34',
+    tags: 'Farro, Sunchokes, Citrus',
+  },
+  {
+    title: 'Brick Pressed Jerk Chicken',
+    price: '$36',
+    tags: 'Carolina Gold Rice, Sweet Potato, Cilantro',
+  },
+  {
+    title: 'Roasted Koginut Squash',
+    price: '$28',
+    tags: 'Rye Berries, Shaved Brussels Sprouts, Pecans, Quince, Savory Granola',
+  },
+];
+
+const awards = [
+  {
+    imgUrl: images.award02,
+    title: 'Bib Gourmond',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award01,
+    title: 'Rising Star',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award05,
+    title: 'AA Hospitality',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award03,
+    title: 'Outstanding Chef',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+];
+
+export default { appetizers, mains, awards };
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
-
+import { SubHeading, MenuItem } from '../../components';
+import { data, images } from '../../constants';
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className="app__specialMenu flex__center section__padding" id="menu">
+    <div className="app__specialMenu-title">
+      <SubHeading title="Menu that fits your palatte" />
+      <h1 className="headtext__cormorant">Today's Special</h1>
+    </div>
+
+    <div className="app__specialMenu-menu">
+      <div className="app__specialMenu-menu_appetizers  flex__center">
+        <p className="app__specialMenu-menu_heading">Appetizers</p>
+        <div className="app__specialMenu_menu_items">
+          {data.appetizers.map((appetizer, index) => (
+            <MenuItem key={appetizer.title + index} title={appetizer.title} price={appetizer.price} tags={appetizer.tags} />
+          ))}
+        </div>
+      </div>
+
+      <div className="app__specialMenu-menu_img">
+        <img src={images.menu} alt="menu__img" />
+      </div>
+
+      <div className="app__specialMenu-menu_mains  flex__center">
+        <p className="app__specialMenu-menu_heading">Mains</p>
+        <div className="app__specialMenu_menu_items">
+          {data.mains.map((main, index) => (
+            <MenuItem key={main.title + index} title={main.title} price={main.price} tags={main.tags} />
+          ))}
+        </div>
+      </div>
+    </div>
+
+    <div style={{ marginTop: 15 }}>
+      <button type="button" className="custom__button">View More</button>
+    </div>
   </div>
 );
 
